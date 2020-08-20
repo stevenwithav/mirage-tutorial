21cfe8886ee9e8a49477fafa60818af4ac27e1a8
nettlesoup <sanettleingham@hotmail.co.uk>
2020-08-20 10:53:33 +0100

    Setup initial Mirage route handler

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-// Welcome to the tutorial!
+import { createServer } from 'miragejs';
+
+export default function () {
+	createServer({
+		routes() {
+			this.get('/api/reminders', () => ({
+				reminders: [
+					{ id: 1, text: 'Reminder 1' },
+					{ id: 2, text: 'Reminder 2' },
+					{ id: 3, text: 'Reminder 3' },
+				],
+			}));
+		},
+	});
+}
