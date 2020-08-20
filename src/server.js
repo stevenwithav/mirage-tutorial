690c0efd071124bc8ec5643ed2d73e07ed3798f9
nettlesoup <sanettleingham@hotmail.co.uk>
2020-08-20 11:29:49 +0100

    Seed initial reminders

--- src/server.js
+++ src/server.js
@@ -6,6 +6,12 @@ export default function () {
 			reminder: Model,
 		},
 
+		seeds(server) {
+			server.create('reminder', { text: 'Reminder 1' });
+			server.create('reminder', { text: 'Reminder 2' });
+			server.create('reminder', { text: 'Reminder 3' });
+		},
+
 		routes() {
 			this.get('/api/reminders', (schema) => {
 				// console.log(schema);
