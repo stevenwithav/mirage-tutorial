1e45162d24003ddb59563f0a3628f375179abb19
nettlesoup <sanettleingham@hotmail.co.uk>
2020-08-20 15:13:34 +0100

    Add additional tests

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -46,7 +46,7 @@ test('it can add a  reminder to a list', async () => {
 	await userEvent.type(screen.getByTestId('new-reminder-text'), 'New Reminder');
 	userEvent.click(screen.getByTestId('save-new-reminder'));
 
-	screen.debug();
+	// screen.debug();
 
 	await waitForElementToBeRemoved(() =>
 		screen.getByTestId('new-reminder-text')
@@ -54,4 +54,84 @@ test('it can add a  reminder to a list', async () => {
 	expect(screen.getByText('New Reminder')).toBeInTheDocument();
 	expect(server.db.reminders.length).toEqual(1);
 	expect(server.db.reminders[0].listId).toEqual(list.id);
+});
+
+test('the all acreen shows list tags', async () => {
+	server.create('reminder', { text: 'Reminder 1' });
+
+	const list = server.create('list', { name: 'List 1' });
+	server.create('reminder', { text: 'Reminder 2', list });
+
+	visit('/');
+	await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+	const [reminder1, reminder2] = screen.getAllByTestId('reminder');
+
+	expect(reminder1).toHaveTextContent('Reminder 1');
+	expect(reminder2).toHaveTextContent('Reminder 2');
+	expect(reminder2.querySelector('[data-testid=list-tag]')).toHaveTextContent(
+		'List 1'
+	);
+});
+
+test('it can delete a reminder', async () => {
+	server.create('reminder', { text: 'Reminder to delete' });
+	visit('/');
+
+	await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+	userEvent.click(screen.getByTestId('delete-reminder'));
+
+	expect(screen.queryByText('Reminder to delete')).not.toBeInTheDocument();
+	expect(server.db.reminders.length).toEqual(0);
+});
+
+test('it can create a list', async () => {
+	visit('/');
+	await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+	userEvent.click(screen.getByTestId('toggle-sidebar'));
+	userEvent.click(screen.getByTestId('add-list'));
+	await userEvent.type(screen.getByTestId('new-list-text'), 'Home');
+	userEvent.click(screen.getByTestId('save-new-list'));
+
+	await waitForElementToBeRemoved(() => screen.getByTestId('new-list-text'));
+
+	expect(screen.getByTestId('active-list-title')).toHaveTextContent('Home');
+	expect(server.db.lists.length).toEqual(1);
+});
+
+test('a list shows only its own reminders', async () => {
+	server.create('reminder');
+
+	const list = server.create('list', {
+		reminders: server.createList('reminder', 3),
+	});
+
+	visit(`/${list.id}?open`);
+	await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+	expect(screen.getAllByTestId('reminder').length).toEqual(3);
+});
+
+test('it can delete a list', async () => {
+	server.create('reminder');
+
+	const list = server.create('list', {
+		reminders: server.createList('reminder', 3),
+	});
+
+	visit(`/${list.id}?open`);
+	await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+	userEvent.click(screen.getByTestId('delete-list'));
+
+	await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+
+	expect(screen.getByTestId('active-list-title')).toHaveTextContent(
+		'Reminders'
+	);
+	expect(screen.getAllByTestId('reminder').length).toEqual(1);
+	// No lists should remain
+	expect(server.db.lists.length).toEqual(0);
+	// Only one reminder should remain
+	expect(server.db.reminders.length).toEqual(1);
 });
